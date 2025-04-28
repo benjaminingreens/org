5595a90646d6513f0c9cea54c4c95b4dba2fac2a
benjaminingreens <benjaminphammond@gmail.com>
2025-04-28 14:45:20 +0100

    testing structure

--- org/setup.py
+++ org/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="org",
-    version="0.0.11",
+    version="0.0.12",
     description="Suckless second brain",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
