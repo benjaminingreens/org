45fbd9b44bf399d14487793ae0367cf4266621f0
benjaminingreens <benjaminphammond@gmail.com>
2025-04-30 16:47:55 +0100

    using ruamel

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="org",
-    version="0.0.16",
+    version="0.0.17",
     description="Suckless second brain",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
