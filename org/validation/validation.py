359f8f57e1cfc789e256c8d61256861918e624d8
benjaminingreens <benjaminphammond@gmail.com>
2025-06-23 20:56:24 +0100

    Auto-commit: 2025-06-23 - 20:56:24

---
+++ org/validation/validation.py
@@ -0,0 +1,40 @@
+import os
+import sys
+
+def log():
+
+    return None
+
+def metadata_format_validation():
+    """
+    Describe function
+    """
+
+    # initialise dictonary for file information
+    file_info = {
+        "file_path": None,
+        "directory": None,
+        "category": None,
+        "valid": None,
+    }
+
+    return None
+
+def metadata_content_validation():
+
+    return None
+
+def main():
+
+    # PC: log validation start
+
+    # PC: check if .org/ is present
+    # if not, raise error and prompt user to run org init
+
+    x = metadata_format_validation()
+
+    y = metadata_content_validation()
+
+    # PC: log validation end
+
+    return None
