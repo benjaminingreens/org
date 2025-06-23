e157d57ec1d440d03049fd64fa26718009e82bfb
benjaminingreens <benjaminphammond@gmail.com>
2025-06-24 00:33:31 +0100

    Auto-commit: 2025-06-24 - 00:33:31

--- org/validation/validation.py
+++ org/validation/validation.py
@@ -5,11 +5,31 @@ from org.logging.logging import log
 
 cwd = os.getcwd()
 
+def grab_invalid_files():
+    """
+    1. Walk through files
+    2. If any do not begin with 'RULE VIOLATION', store filepath
+    ...
+    """
+
+    log("info", "Looking for previously invalidated files")
+
+    # 1. walking through files
+    # FIXME: This will not work unless invalid is automatically created
+    # by a later function
+    for root, _, files in os.walk(cwd + "/invalid"):
+        for name in files:
+            path = os.path.join(root, name)
+
+    return None
+
 def metadata_format_validation():
     """
     Function enforces Org spec format rules:
 	1.1, 1.2, 2.1, 2.2, 3.1, 3.2, 4.1, 4.2, 4.3, 4.4, & 5.1
     """
+
+    log("info", "Beginning format validation process")
 
     # 1. initialise dictonary for file information
     file_info = {
@@ -21,6 +41,8 @@ def metadata_format_validation():
 		
     # 2. look for 'invalid' folder
     if os.path.isdir('invalid'):
+        grab_invalid_files()
+    else:
         pass
 
     return None
