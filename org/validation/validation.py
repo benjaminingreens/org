09078b0e0ffba8e449741177e7451d1e966e9f38
benjaminingreens <benjaminphammond@gmail.com>
2025-06-24 00:12:54 +0100

    Commit: 2025-06-24 00:12:54

--- org/validation/validation.py
+++ org/validation/validation.py
@@ -1,27 +1,14 @@
 import os
 import sys
 import datetime
+from org.logging.logging import log
 
-# CONSTANTS
 cwd = os.getcwd()
-log = os.path.join(os.getcwd(), "log.txt")
-
-def log(level: str, message: str) -> None:
-		"""
-		Just my personal log function
-		"""
-		
-		valid_levels: {"debug", "info", "warning", "error", "critical"}
-		
-		current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    script_name = os.path.basename(__file__)
-    with open(LOG_PATH, "a") as f:
-        f.write(f"[{current_time}][{script_name}]: {message}\n")
 
 def metadata_format_validation():
     """
     Function enforces Org spec format rules:
-		1.1, 1.2, 2.1, 2.2, 3.1, 3.2, 4.1, 4.2, 4.3, 4.4, & 5.1
+	1.1, 1.2, 2.1, 2.2, 3.1, 3.2, 4.1, 4.2, 4.3, 4.4, & 5.1
     """
 
     # 1. initialise dictonary for file information
@@ -32,9 +19,9 @@ def metadata_format_validation():
         "valid": None,
     }
 		
-		# 2. look for 'invalid' folder
-		if os.path.isdir('invalid'):
-			pass
+    # 2. look for 'invalid' folder
+    if os.path.isdir('invalid'):
+        pass
 
     return None
 
@@ -44,7 +31,7 @@ def metadata_content_validation():
 
 def main():
 
-    log("Validation start")
+    log("info", "Validation start")
 
     # PC: check if .org/ is present
     # if not, raise error and prompt user to run org init
