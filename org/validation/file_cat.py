211ed72a9d367e4923b9aa554eb1268f46892f66
benjaminingreens <benjaminphammond@gmail.com>
2025-06-24 14:50:30 +0100

    working on validation script

--- org/validation/file_cat.py
+++ org/validation/file_cat.py
@@ -1,4 +1,12 @@
+"""
+file_cat.py
+
+Outlines supported file types and any commenting syntax.
+Gets the file type data for a given file and returns it.
+"""
+
 import os
+from org.logging.logging import log
 
 free_file_types = {
     ".txt",
@@ -9,23 +17,84 @@ friendly_file_types = {
 }
 
 escapable_file_types = {
-    ".py": "#",
-    ".c": "//",
-    ".js": "//",
-    ".ts": "//",
-    ".sh": "#",
-    ".bash": "#",
-    ".rb": "#",
-    ".php": {"//", "#"},
-    ".go": "//",
-    ".cpp": "//",
-    ".jsonc": {"//"},
-    ".yaml": "#",
-    ".yml": "#",
-    ".toml": "#",
-    ".ini": {";", "#"},
-    ".cfg": {";", "#"},
-    ".tex": "%",
+    # Programming languages
+    ".py": {"line": "#"},
+    ".c": {"line": "//", "block": ("/*", "*/")},
+    ".cpp": {"line": "//", "block": ("/*", "*/")},
+    ".js": {"line": "//", "block": ("/*", "*/")},
+    ".ts": {"line": "//", "block": ("/*", "*/")},
+    ".java": {"line": "//", "block": ("/*", "*/")},
+    ".go": {"line": "//", "block": ("/*", "*/")},
+    ".rs": {"line": "//", "block": ("/*", "*/")},
+    ".rb": {"line": "#", "block": ("=begin", "=end")},
+    ".php": {"line": ["//", "#"], "block": ("/*", "*/")},
+    ".sh": {"line": "#"},
+    ".bash": {"line": "#"},
+    ".pl": {"line": "#"},
+
+    # Markup/config/data formats
+    ".yaml": {"line": "#"},
+    ".yml": {"line": "#"},
+    ".toml": {"line": "#"},
+    ".ini": {"line": [";", "#"]},
+    ".cfg": {"line": [";", "#"]},
+    ".jsonc": {"line": "//", "block": ("/*", "*/")},
+    ".xml": {"block": ("<!--", "-->")},
+    ".html": {"block": ("<!--", "-->")},
+    ".tex": {"line": "%"},
+
+    # Script / automation
+    ".make": {"line": "#"},
+    "Makefile": {"line": "#"},
+    ".dockerfile": {"line": "#"},
+    "Dockerfile": {"line": "#"},
+
+    # Misc
+    ".rst": {"line": ".."},
+    ".bat": {"line": "REM"},
+    ".ps1": {"line": "#"},
 }
 
-# what about block syntax?
+def get_file_type_data(filepath: str) -> dict:
+
+    log("info", f"Getting file type data for file '{filepath}'")
+
+    file_type_data = {}
+
+    # get file type data
+    _, ext = os.path.splitext(filepath)
+
+    log("debug", f"Filetype for '{filepath}' is: '{ext}'")
+
+    if ext in free_file_types:
+        file_type_data = {
+            "filetype": ext,
+            "filecat": "free",
+        }
+    elif ext in friendly_file_types:
+        file_type_data = {
+            "filetype": ext,
+            "filecat": "friendly",
+        }
+    elif ext in escapable_file_types:
+        entry = escapable_file_types[ext]
+        file_type_data = {
+            "filetype": ext,
+            "filecat": "free",
+            "line_comment": entry.get("line"),
+            "block_comment_open": entry.get("block", (None, None))[0],
+            "block_comment_close": entry.get("block", (None, None))[1],
+        }
+    else:
+        file_type_data = {
+            "filetype": ext,
+            "filecat": "not supported",
+        }
+        log("warning", f"Filetype '{ext}' of file '{filepath}' is not supported")
+
+    log("debug", f"Filetype data is: {file_type_data}")
+
+    if not file_type_data:
+        log("error", "Filetype data dict is empty")
+
+    return file_type_data
--- org/validation/validation.py
+++ org/validation/validation.py
@@ -1,31 +1,84 @@
+"""
+validation.py
+
+Validates files for Org according to the Org specification.
+"""
+
 import os
 import sys
 import datetime
+import sqlite3
 from org.logging.logging import log
+from org.validation.file_cat import get_file_type_data
 
+# TODO: cwd needs to be dynamic so Org can be run from anywehre.
+# If Org is run in a random location that isn't default cwd,
+# config should find default cwd, set it to cwd,
+# and pass this to all functions
 cwd = os.getcwd()
+workspace = os.path.basename(cwd)
 
-# NOTE: What about defining acceptable file types and then reading them?
-
-def grab_invalid_files():
+def grab_invalid_files() -> list:
     """
-    1. Walk through files
-    2. If any do not begin with 'RULE VIOLATION', store filepath
-    ...
+    Finds invalid files ready for re-validation.
+    Grabs their validation metadata from the sql db
+    and returns them as a list of dicts.
     """
 
     log("info", "Looking for previously invalidated files")
 
-    # 1. walking through files
-    # FIXME: This will not work unless invalid is automatically created
-    # by a later function
-    #
-    # NOTE: Must be any plaintext file
-    for root, _, files in os.walk(cwd + "/invalid"):
-        for name in files:
-            path = os.path.join(root, name)
+    row_dict = {}
+    row_dicts = []
 
-    return None
+    # 1. connect to the database for invalid
+    invalid_db = os.path.join(cwd, "invalid", ".invalid.db")
+    cx = sqlite3.connect(invalid_db)
+    cursor = cx.cursor()
+
+    # 2. walking through files
+    paths = (
+        os.path.join(root, name)
+        for root, _, files in os.walk(cwd + "/invalid")
+        for name in files
+    )
+
+    counter = 0
+    for path in paths:
+
+        # 3. get relative path for better logs etc.
+        if path.startswith(cwd):
+            rel_path = path[len(cwd):].lstrip("/")
+        else:
+            rel_path = path
+
+        # 4. check file is supported by Org
+        file_type_data = get_file_type_data(rel_path)
+        if file_type_data.get("filecat") == "not supported":
+            continue
+
+        # 5. check first line of file for 'RULE VIOLATION'
+        # (this must be removed by user if they want to validate the file)
+        with open(path, 'r', encoding='utf-8') as f:
+            first_line = f.readline()
+        if 'RULE VIOLATION' in first_line:
+            log("debug", f"File '{rel_path}' is still invalid")
+            continue
+        else:
+            counter += 1
+            log("debug", f"File '{rel_path}' ready for re-validation")
+
+        # 6. get the sql data from the <invalid.db>
+        ## pop row from db and store it
+        cursor.execute("SELECT * FROM files WHERE file_path = ?", (path,))
+        cursor.execute("DELETE FROM files WHERE file_path = ?", (path,))
+        row = cursor.fetchone()
+        row_dict = dict(row)
+        row_dicts.append(row_dict)
+        cx.commit()
+
+    log("info", f"Found {counter} previously invalid files for re-validation")
+
+    return row_dicts
 
 def metadata_format_validation():
     """
@@ -43,11 +96,6 @@ def metadata_format_validation():
         "valid": None,
     }
 		
-    # 2. look for 'invalid' folder
-    if os.path.isdir('invalid'):
-        grab_invalid_files()
-    else:
-        pass
 
     return None
 
@@ -57,15 +105,28 @@ def metadata_content_validation():
 
 def main():
 
-    log("info", "Validation start")
+    log("info", f"Validation start for workspace '{workspace}")
+
+    file_validation_metadata = []
 
     # PC: check if .org/ is present
     # if not, raise error and prompt user to run org init
+
+    # PC: function for getting sql db from all folders
+    # PC: function for getting scan of filesystem
+
+    # 2. look for 'invalid' folder and get files for revalidation
+    if os.path.isdir('invalid'):
+        file_revalidation_metadata = grab_invalid_files()
+    else:
+        file_revalidation_metadata = []
 
     x = metadata_format_validation()
 
     y = metadata_content_validation()
 
-    # PC: log validation end
+    # PC: If invalid folder empty and invalid.db empty, delete
+
+    log("info", f"Validation end for workspace '{workspace}")
 
     return None
